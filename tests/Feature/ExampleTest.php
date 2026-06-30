169e78a8d4d9b6979a2930b3fffea9d454d2be28
mamun-zavisoft <mamun.zavisoft@gmail.com>
2026-06-30 17:38:33 +0600

    CICD pipeline file make and upload

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,8 +11,8 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-
     use RefreshDatabase;
+
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');

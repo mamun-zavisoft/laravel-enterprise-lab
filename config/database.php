ff9080fc1b638f2725bbaae03d3334f4f71e63a9
mamun-zavisoft <mamun.zavisoft@gmail.com>
2026-07-01 16:45:31 +0600

    file update

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'testing'),
-            'username' => env('DB_USERNAME', 'root'),
+            'username' => env('DB_USERNAME', 'laravel'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
